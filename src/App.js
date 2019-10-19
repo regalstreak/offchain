8aff52aface93f7a0f6d3c9022f83d139925f2ad
= <anirudh.b.123@gmail.com>
2019-10-20 04:00:26 +0530

    sms can now be sent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import SendSMS from 'react-native-sms-x';
 
 const App = () => {
+  const [ethAddress, setEthAddress] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const sendSms = () => {
+    SendSMS.send(
+      123,
+      '9920765114',
+      'Hey.., this is me!\nGood to see you. Have a nice day.',
+      (msg) => {
+        console.log(msg);
+      });
+  };
   return (
     <View style={{ margin: 16 }}>
-      <Text style={{ fontSize: 36, fontWeight: '700', marginVertical: 16, }}>Offchain</Text>
+      <Text style={{ fontSize: 36, fontWeight: '700', marginVertical: 16 }}>Offchain</Text>
 
       <View style={{ borderWidth: 1, marginVertical: 16, borderRadius: 8, paddingHorizontal: 4 }}>
         <TextInput
-          placeholder='Enter ethereum address here'
+          placeholder="Enter ethereum address here"
+          onChange={address => {
+            setEthAddress(address);
+          }}
+        />
+      </View>
+
+      <View style={{ borderWidth: 1, marginVertical: 16, borderRadius: 8, paddingHorizontal: 4 }}>
+        <TextInput
+          placeholder="Enter amount to be sent"
+          onChange={amt => {
+            setAmount(amt);
+          }}
         />
       </View>
 
       <TouchableOpacity
         style={{ padding: 16, backgroundColor: '#AAAAAA', marginVertical: 16, borderRadius: 8 }}
-        onPress={() => { console.log('test') }}>
+        onPress={() => { sendSms(); }}>
         <Text >Send</Text>
       </TouchableOpacity>
     </View>
